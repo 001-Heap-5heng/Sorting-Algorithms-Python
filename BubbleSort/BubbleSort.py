91955c7e37081c93b324414ae1d856f084c0c5f7
HeapShengOoi <heapsheng_ooi@yahoo.com>
2020-03-12 21:07:25 +0800

    Added performance matrix to bubble sort.

--- BubbleSort/BubbleSort.py
+++ BubbleSort/BubbleSort.py
@@ -6,14 +6,19 @@ print(A)
 print("\n")
 print("Starting Bubble Sort: ")
 
+totalComparison = 0
+totalSwaps = 0
+
 for h in range(0, len(A)):
     sorted = True
     for i in range(0, len(A)-h-1):
+        totalComparison = totalComparison + 1
         if A[i] > A[i+1]:
             sorted = False
             temp = A[i]
             A[i] = A[i+1]
             A[i+1] = temp
+            totalSwaps = totalSwaps + 1
             print(A)
     if sorted:
         break
@@ -23,3 +28,7 @@ print("\n")
         
 print("Sorted Characters: ")
 print(A)
+
+print("Number of Sorted Elements: ", len(A))
+print("Total Comparisons Made: ", totalComparison)
+print("Total Swaps Made: ", totalSwaps)
